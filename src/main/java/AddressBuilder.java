8e1fcac3490129c5540345d2494e0077975856f9
BryanG2442 <bryangarcia2442@gmail.com>
2024-10-03 10:25:23 -0700

    AWDqwx

--- src/main/java/AddressBuilder.java
+++ src/main/java/AddressBuilder.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class AddressBuilder {
+    static public void main (String[] args) {
+        String firstName = "Classmate";
+        firstName = firstName.toUpperCase();
+        System.out.println("Hey " + firstName + ", you're great!");
+    }
 }
